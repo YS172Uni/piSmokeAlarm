88222a5f28edc6f8c7c4961bec211facf2a7b8e1
Jackson Green <87842000+JacksonGreenNZ@users.noreply.github.com>
2025-10-16 15:23:48 +1300

    functions and multithreading sensor_mqtt.py
    used multithreading to handle sensor checks and control pi messages
    finished moving mqtt setup into functions

--- sensor_pi_files/sensor_mqtt.py
+++ sensor_pi_files/sensor_mqtt.py
@@ -3,6 +3,7 @@ import RPi.GPIO as G
 import time
 import json
 import uuid
+import threading
 from datetime import datetime
 import paho.mqtt.client as mqtt
 from gpiozero import TonalBuzzer
@@ -16,57 +17,69 @@ G.setmode(G.BCM)
 G.setup(SENSOR_PIN, G.IN)
 alarm = TonalBuzzer(SPEAKER_PIN)
 
+def alarm_on(buzzer):
+    buzzer.play(Tone(640))
+    sleep(1.25)
+    buzzer.play(Tone(600))
+    sleep(1.25)
+    buzzer.play(Tone(640))
+    sleep(1.25)
+    buzzer.play(Tone(600))
+    sleep(1.25)
+
 # MQTT Setup
 BROKER = "10.62.134.146"  #Control Pi IP
 PORT = 1883
 USER = "sensor"
 PASSWORD = "pi2025"
+    
+def create_mqtt_client(NODE_ID):#setup pi for mqtt
+    client = mqtt.Client(client_id=f"sensor-{NODE_ID}")
+    client.username_pw_set(USER, PASSWORD)
+    return client
 
-NODE_ID = f"node-{uuid.uuid4()}"  #sensor node ID
+def connect_subscribe(client, NODE_ID):#connect/subscribe to control pi
+    client.on_message = on_message
+    client.connect(BROKER, PORT, 60)
+    client.subscribe(f"control/{NODE_ID}", qos=1)
+    client.loop_start() 
+    print(f"{datetime.now()}: Connected to Control Pi")
 
-client = mqtt.Client(client_id=f"sensor-{NODE_ID}")
-client.username_pw_set(USER, PASSWORD)
 
-# Connect to broker
-client.connect(BROKER, PORT, 60)
-
-def alarm_on(buzzer):
-    buzzer.play(Tone(640))
-    sleep(1.25)
-	buzzer.play(Tone(600))
-	sleep(1.25)
-    buzzer.play(Tone(640))
-	sleep(1.25)
-	buzzer.play(Tone(600))
-	sleep(1.25)
-
-# Callback for control messages
-def on_message(client, userdata, msg):
+def on_message(client, userdata, msg):# Callback for control messages
     msg_payload = msg.payload.decode()
     print(f"{datetime.now()}: Control message received -> {msg_payload}")
+    instruction_received.set()
     if msg_payload == "ALARM":
-        alarm_on(buzzer)
+        alarm_instruction.set()
+    elif msg_payload == "CLEAR":
+        alarm_instruction.clear()
 
-client.on_message = on_message
-client.subscribe(f"control/{NODE_ID}", qos=1)
-client.loop_start()  # Run network loop in background
-
-def publish_data(client, NODE_ID, detected):
+def publish_data(client, NODE_ID, detected):#send data to control pi
     payload = json.dumps({"node": NODE_ID, "detected": detected})
     client.publish("sensors/" + NODE_ID, payload, qos=1)
 
-# Main sensor loop
+#                   Main sensor loop
+NODE_ID = f"node-{uuid.uuid4()}"  #sensor node ID
+client = create_mqtt_client(NODE_ID)
+connect_subscribe(client, NODE_ID)
+alarm_instruction = threading.Event()
+instruction_received = threading.Event()
+
 try:
     while True:
+        if alarm_instruction.is_set():
+            alarm_on(buzzer)
         detected = 1 if G.input(SENSOR_PIN) == G.HIGH else 0
         if detected:
             print(f"{datetime.now()}: Gas Detected")
             publish_data(client, NODE_ID, detected)
-            #play tone for 5 seconds
-            alarm_on(buzzer)
+            instruction_received.wait()
+            instruction_received.clear()
         else:
             print(f"{datetime.now()}: Nothing Detected")        
             publish_data(client, NODE_ID, detected)
+        if not alarm_instruction.is_set():
             time.sleep(5)  #check every x seconds
 
 except KeyboardInterrupt:
