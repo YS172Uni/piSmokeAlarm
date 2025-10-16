2ca10cfc45e40cf5744e3aade3acbddec168cde1
Jackson Green <87842000+JacksonGreenNZ@users.noreply.github.com>
2025-10-16 16:05:53 +1300

    Update sensor_mqtt.py for local/remote modes
    Added local/remote modes and handshakes

--- sensor_pi_files/sensor_mqtt.py
+++ sensor_pi_files/sensor_mqtt.py
@@ -33,19 +33,14 @@ BROKER = "10.62.134.146"  #Control Pi IP
 PORT = 1883
 USER = "sensor"
 PASSWORD = "pi2025"
+alarm_instruction = threading.Event()
+instruction_received = threading.Event()
+connected_event = threading.Event()
     
 def create_mqtt_client(NODE_ID):#setup pi for mqtt
     client = mqtt.Client(client_id=f"sensor-{NODE_ID}")
     client.username_pw_set(USER, PASSWORD)
     return client
-
-def connect_subscribe(client, NODE_ID):#connect/subscribe to control pi
-    client.on_message = on_message
-    client.connect(BROKER, PORT, 60)
-    client.subscribe(f"control/{NODE_ID}", qos=1)
-    client.loop_start() 
-    print(f"{datetime.now()}: Connected to Control Pi")
-
 
 def on_message(client, userdata, msg):# Callback for control messages
     msg_payload = msg.payload.decode()
@@ -56,6 +51,33 @@ def on_message(client, userdata, msg):# Callback for control messages
     elif msg_payload == "CLEAR":
         alarm_instruction.clear()
 
+def on_connect(client, userdata, flags, rc):
+    NODE_ID = userdata
+    if rc == 0:
+        print(f"{datetime.now()}: Connected to Control Pi")
+        client.subscribe(f"control/{NODE_ID}", qos=1)
+        connected_event.set()
+    else:
+        print(f"{datetime.now()}: Could not connect to Control Pi - rc = {rc}")
+
+def connect_subscribe(client, NODE_ID):
+    print("Attempting Connection with Control Pi")
+    client.on_message = on_message
+    client.on_connect = on_connect
+    client.user_data_set(NODE_ID)
+    client.loop_start()
+    try:
+        client.connect(BROKER, PORT, 60)
+    except Exception as e:
+        print(f"MQTT connection failed: {e}")
+
+    connected = connected_event.wait(timeout=10)
+    if connected:
+        print("Remote Control Mode")
+    else:
+        print("Local Mode")
+    return connected
+
 def publish_data(client, NODE_ID, detected):#send data to control pi
     payload = json.dumps({"node": NODE_ID, "detected": detected})
     client.publish("sensors/" + NODE_ID, payload, qos=1)
@@ -63,25 +85,33 @@ def publish_data(client, NODE_ID, detected):#send data to control pi
 #                   Main sensor loop
 NODE_ID = f"node-{uuid.uuid4()}"  #sensor node ID
 client = create_mqtt_client(NODE_ID)
-connect_subscribe(client, NODE_ID)
-alarm_instruction = threading.Event()
-instruction_received = threading.Event()
+connected = connect_subscribe(client, NODE_ID)
 
 try:
-    while True:
-        if alarm_instruction.is_set():
-            alarm_on(alarm)
-        detected = 1 if G.input(SENSOR_PIN) == G.HIGH else 0
-        if detected:
-            print(f"{datetime.now()}: Gas Detected")
-            publish_data(client, NODE_ID, detected)
-            instruction_received.wait()
-            instruction_received.clear()
-        else:
-            print(f"{datetime.now()}: Nothing Detected")        
-            publish_data(client, NODE_ID, detected)
-        if not alarm_instruction.is_set():
-            time.sleep(5)  #check every x seconds
+    if connected:
+        while True:
+            if alarm_instruction.is_set():
+                alarm_on(alarm)
+            detected = 1 if G.input(SENSOR_PIN) == G.HIGH else 0
+            if detected:
+                print(f"{datetime.now()}: Gas Detected")
+                publish_data(client, NODE_ID, detected)
+                instruction_received.wait()
+                instruction_received.clear()
+            else:
+                print(f"{datetime.now()}: Nothing Detected")        
+                publish_data(client, NODE_ID, detected)
+            if not alarm_instruction.is_set():
+                time.sleep(5)  #check every x seconds
+    else:
+        while True:
+            detected = 1 if G.input(SENSOR_PIN) == G.HIGH else 0
+            if detected:
+                print(f"{datetime.now()}: Gas Detected")
+                alarm_on(alarm)
+            else:
+                print(f"{datetime.now()}: Nothing Detected")        
+                time.sleep(5)  #check every x seconds
 
 except KeyboardInterrupt:
     print("Exiting sensor script")
